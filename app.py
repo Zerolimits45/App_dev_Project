86313c7fa7491a0d9a33ce6cc50e4cc6ad7e5027
Aloysius <zerolimits45@gmail.com>
2022-12-26 23:33:34 +0800

    Created login page and forms.py

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from forms import loginForm
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
 @app.route('/shop')
 def shop():
     return render_template('shop.html')
+
+
+@app.route('/login')
+def login():
+    form = loginForm(request.form)
+    return render_template('login.html', form=form)
+
+
 if __name__ == '__main__':
     app.run()
-
--- forms.py
+++ forms.py
@@ -1,5 +1,13 @@
 from wtforms import *
 
 
-class loginUser(Form):
-    email = EmailField("Email Address", [validators.Email(granular_message=True)], )
+# Login for users
+class loginForm(Form):
+    email = EmailField("Email Address", [
+        validators.Email(granular_message=True),
+        validators.DataRequired(message="Please input your email")
+    ])
+    password = PasswordField("Password", [
+        validators.Length(6, 64, message="Password must be at least 6 characters"),
+        validators.DataRequired(message="Please input your password")
+    ])
