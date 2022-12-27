d083e39056b8415557dd27ec50481ff13f1eda20
TanBenneT <tanbennet02@gmail.com>
2022-12-27 17:56:34 +0800

    Added Sign Up Function

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-from forms import LoginForm
+from forms import *
+from user import *
 import shelve
 from flask_wtf.csrf import CSRFProtect  # up for debate
 
@@ -44,9 +45,36 @@ def login():
                     return redirect(url_for('home'))
     return render_template('login.html', form=form)
 
-@app.route('/signup')
+
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template('login.html', form=form)
+    form = SignUpForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user_dict = {}
+        db = shelve.open('Users')
+        try:
+            if 'User' in db:
+                user_dict = db['User']
+            else:
+                db['User'] = user_dict
+        except:
+            print("Error in retrieving Users from storage.")
+
+        email = form.email.data
+        password = form.password.data
+        name = form.name.data
+        count = len(user_dict)
+
+        user = User(email, password, name, count)
+
+        user_dict[user.get_uid()] = user
+        db['User'] = user_dict
+
+        db.close()
+
+        return redirect(url_for('login'))
+    return render_template('signUp.html', form=form)
+
 
 @app.route('/logout')
 def logout():
