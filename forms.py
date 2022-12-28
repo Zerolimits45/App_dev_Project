31b5ca3d1c964cdcce26851774b39d107ba854fa
Aloysius <zerolimits45@gmail.com>
2022-12-28 18:21:12 +0800

    contact form html

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
-from wtforms import Form, BooleanField, PasswordField, StringField, SelectField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
+from wtforms import Form, BooleanField, PasswordField, StringField, SelectField, SubmitField, TextAreaField, TelField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length, regexp
 import shelve
 
 
@@ -43,8 +43,7 @@ class LoginForm(Form):
 class SignUpForm(Form):
     name = StringField('Name', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=64,
-                                                                            message="Password needs to be at least 6 characters long.")])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=64, message="Password needs to be at least 6 characters long.")])
     confirmPassword = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
@@ -63,3 +62,11 @@ class SignUpForm(Form):
         for key in user_dict:
             if self.email.data == user_dict[key].get_email():
                 raise ValidationError('Email is already in use.')
+
+#Contact Form
+class ContactForm(Form):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    phone = StringField("Phone Number", validators=[regexp("^[689]\d{7}$", message="Please enter a valid phone number")])
+    feedback = TextAreaField('Explain your Problem', validators=[DataRequired(), Length(min=1, max=1024, message="Please leave a message within 1024 characters")])
+    submit = SubmitField('Send Feedback')
