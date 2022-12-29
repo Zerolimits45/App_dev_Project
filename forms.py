cfb52ed9ab5dec72c8afcd21fc4b14a85c7c2827
Aloysius <zerolimits45@gmail.com>
2022-12-30 01:34:08 +0800

    new address class and address form

--- forms.py
+++ forms.py
@@ -1,4 +1,5 @@
-from wtforms import Form, BooleanField, PasswordField, StringField, SelectField, SubmitField, TextAreaField, TelField, validators, IntegerField
+from wtforms import Form, BooleanField, PasswordField, StringField, SelectField, SubmitField, TextAreaField, TelField, \
+    validators, IntegerField, DateField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length, regexp
 import shelve
 
@@ -6,9 +7,7 @@ import shelve
 # Login for users
 class LoginForm(Form):
     email = StringField('Email', validators=[DataRequired(message='Please input your email'), Email()])
-    password = PasswordField('Password', validators=[DataRequired(message='Please input your password'),
-                                                     Length(min=6, max=64,
-                                                            message='Password must be at least 6 characters')])
+    password = PasswordField('Password', validators=[DataRequired(message='Please input your password'), Length(min=6, max=64,message='Password must be at least 6 characters')])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Log In')
 
@@ -94,10 +93,10 @@ class EditProductForm(Form):
 
 # Contact Form
 class ContactForm(Form):
-    name = StringField('Name', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired(message='Please input your name')])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    phonenumber = StringField("Phone Number", validators=[regexp("^[689]\d{7}$", message="Please enter a valid phone number")])
-    message = TextAreaField('Explain your Problem', validators=[DataRequired(), Length(min=1, max=1024, message="Please leave a message within 1024 characters")])
+    phonenumber = StringField("Phone Number",validators=[regexp("^[689]\d{7}$", message="Please enter a valid phone number")])
+    message = TextAreaField('Explain your Problem', validators=[DataRequired(), Length(min=1, max=1024,message="Please leave a message within 1024 characters")])
     submit = SubmitField('Send Feedback')
 
 
@@ -127,3 +126,9 @@ class EditUserForm(Form):
         for key in user_dict:
             if self.email.data == user_dict[key].get_email() and self.email.data != current_user:
                 raise ValidationError('Email is already in use.')
+
+
+# Shipping Form
+class AddressForm(Form):
+    addressName = StringField('Nickname', validators=[DataRequired(message="Please enter a nickname for the Address")])
+    address = StringField('Address', validators=[DataRequired(message="Please enter an address")])
