6e6f83a7cc113bffd474b15c5fde4f40171b814d
TanBenneT <tanbennet02@gmail.com>
2022-12-31 11:35:50 +0800

    Major Changes to Address, Need Install geocoder

--- Address.py
+++ Address.py
@@ -1,23 +1,14 @@
-from geopy.geocoders import ArcGIS
-from geopy.exc import GeopyError
-from flask import flash
-
-
 class Address:
-    def __init__(self, name, location):
-        try:
-            geoLocater = ArcGIS(user_agent="TimeVault")
-            geoLocation = geoLocater.geocode(location)
-        except GeopyError:
-            self.latitude = 0
-            self.longitude = 0
-            flash("Error finding the location provided. Please provide a valid address.", category="Error")
-        else:
-            self.latitude = geoLocation.latitude
-            self.longitude = geoLocation.longitude
-        finally:
-            self.name = name
-            self.location = location
+    def __init__(self, id, name, location, count):
+        self.id = id
+        self.name = name
+        self.location = location
+        self.count = count
+        self.count += 1
+        self.location_id = self.count
+
+    def getid(self):
+        return self.id
 
     def getname(self):
         return self.name
@@ -25,23 +16,17 @@ class Address:
     def getlocation(self):
         return self.location
 
-    def getlatitude(self):
-        return self.latitude
+    def getlocationid(self):
+        return self.location_id
 
-    def getlongitude(self):
-        return self.longitude
+    def setid(self, id):
+        self.id = id
 
     def setname(self, name):
         self.name = name
 
-    def setLocation(self, location):
+    def setlocation(self, location):
         self.location = location
-        try:
-            geoLocater = ArcGIS(user_agent="TimeVault")
-            geoLocation = geoLocater.geocode(location)
-        except GeopyError:
-            self.latitude = 0
-            self.longitude = 0
-        else:
-            self.latitude = geoLocation.latitude
-            self.longitude = geoLocation.longitude
+
+    def setlocationid(self, lid):
+        self.location_id = lid
